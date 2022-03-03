2c14f5aa57b230c4e06e842d6f6b4301e3df4fb9
Takanou <81436875+Takanou@users.noreply.github.com>
2022-03-04 00:02:32 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+//                                                   AUTOROLE
+client.on("guildMemberAdd", member => {
+    client.channels.cache.get(`${config.join_discord_message}`).send(member.displayName + " join Discord ! 🥰 ");
+    member.roles.add(`${config.autorole_roleID}`)
+});
+client.on("guildMemberRemove", member => {
+    client.channels.cache.get(`${config.join_discord_message}`).send(member.displayName + " leave Discord ! 😭 ");
+});
+//AUTOROLE END
